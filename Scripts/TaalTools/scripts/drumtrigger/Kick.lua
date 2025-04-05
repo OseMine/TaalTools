ddee66f3cd12472f706f5a4ea47d93fcf861e001
OseMine <98693947+OseMine@users.noreply.github.com>
2025-04-05 14:49:36 +0200

    Refactor Kick drum trigger function to improve MIDI note handling and initialization

--- Scripts/TaalTools/scripts/drumtrigger/Kick.lua
+++ Scripts/TaalTools/scripts/drumtrigger/Kick.lua
@@ -1,10 +1,8 @@
 -- @description TaalTools - Drum Trigger (Kick)
 -- @author Taal
 -- @version 1.0
--- @about
---   Triggers MIDI note for Kick Drum (MOD Drum)
 
-function triggerKick()
+function Main()
   local track = reaper.GetSelectedTrack(0, 0)
   if not track then
     reaper.ShowMessageBox("Please select a track!", "Error", 0)
@@ -12,9 +10,14 @@ function triggerKick()
   end
   
   -- Send MIDI note for Kick (note 36)
-  reaper.StuffMIDIMessage(0, 0x90, 36, 127)
-  reaper.Sleep(10)  
-  reaper.StuffMIDIMessage(0, 0x80, 36, 0)
+  reaper.StuffMIDIMessage(0, 0x90, 36, 127) -- Note On
+  reaper.defer(function() 
+    reaper.StuffMIDIMessage(0, 0x80, 36, 0) -- Note Off
+  end)
 end
 
-triggerKick()
+if not preset_file_init then
+  local _, file, sec, cmd = reaper.get_action_context()
+  preset_file_init = true
+  Main()
+end
